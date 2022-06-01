5eb032fcdc241ce51ad66b7cf2127eea8008ae7a
Avraham Granov <Avraham.granow@gmail.com>
2022-06-01 17:31:03 +0200

    add address and phone test fields

--- src/test/java/com/shareNow/registrationtest/ShareNowSelenide.java
+++ src/test/java/com/shareNow/registrationtest/ShareNowSelenide.java
@@ -4,8 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class ShareNowSelenide {
 
@@ -14,39 +13,58 @@ public class ShareNowSelenide {
     public void registrationPositivTest() {
         open("https://www.int.share-now.com/");
         Configuration.holdBrowserOpen = true;  //  не закрывать браузер после выполнения теста
-        $("#reggie-link-register-now").click();
+        $(By.id("reggie-link-register-now")).click();
 
 
-        $(By.xpath("//option[contains(text(),'\uD83C\uDDE9\uD83C\uDDEA')]")).click(); //Land
-        $(By.xpath("//option[contains(text(),'Ulm')]")).click();  // Stadt
+        $(By.xpath("//option[@value = 'DE']")).click(); //Land
 
-//        $(By.name("language")).click();  // почему не берёт такой код?
+        $(By.xpath("//option[contains(text(),'Ulm')]")).click();
+
         $(By.xpath("//select[@name='language']")).click();
-        $(By.xpath("//option[contains(text(),'Deutsch')]")).click(); // Sprache // не переключает на немецкий
+        $(By.xpath("//option[contains(text(),'Deutsch')]")).click();
 
-        $(By.xpath("//input[@name='email']")).click();  // Email  // не проходит
-        $(By.xpath("//input[@name='email']")).setValue("emailTest@gmail.com").pressEnter(); // не проходит
+        $(By.xpath("//input[@name='email']")).setValue("emailTest@gmail.com");
 
-        $(By.xpath("//input[@name='password']")).click();  // Password  // не проходит
-        $(By.xpath("//input[@name='password']")).setValue("passwordTest1").pressEnter();
+        $(By.xpath("//input[@name='password']")).setValue("passwordTest1");
 
-        $(By.xpath("//input[@name='pin']")).click();
-        $(By.xpath("//input[@name='pin']")).setValue("passwordTest1").pressEnter();
+        $(By.xpath("//input[@name='pin']")).setValue("passwordTest1");
 
 
         $(By.xpath("//select[@name='salutation']/option[2]")).click();  // titel
 
-        $(By.xpath("//*[@name='firstName']")).click();  // firstName
-        $(By.xpath("//*[@name='firstName']")).setValue("Jonn").pressEnter();
+        $(By.xpath("//input[@name='firstName']")).sendKeys("Jonn");
 
-        $(By.xpath("//*[@name='lastName']")).click();  // lastName
-        $(By.xpath("//input[@name='lastName']")).setValue("Smitt").pressEnter();
+        $(By.xpath("//input[@name='lastName']")).sendKeys("Smitt");
 
         $(By.xpath("//select[@id = 'camelot-select-birth-date-day']/option[18]")).click();
-        $(By.xpath("//select[@id = 'camelot-select-birth-date-month']/option[3]")).click();
-        $(By.xpath("//select[@id = 'camelot-select-birth-date-year']/option[10]")).click();
-        //option[contains(text(),'1973')]
+        $(By.xpath("//select[@id = 'camelot-select-birth-date-month']/option[3]")).click(); //задать месяц явно
+        $(By.xpath("//option[contains(text(),'1973')]")).click();
 
-        $(By.xpath("//select[@name='birthPlace']")).setValue("Deutschland");
+//      $(By.xpath("//input[contains(@name, 'birthPlace')]")).sendKeys("Deutschland");  // код Ивана
+//      $(By.name("birthPlace")).sendKeys("Deutschland"); // почему не проходит ?
+
+        $(By.xpath("//input[@name='birthPlace']")).sendKeys("Deutschland");
+        $(By.xpath("//input[@name='addressStreet']")).sendKeys("Gartenstr,  39");
+        $(By.xpath("//input[@name='addressAdditionalStreet']")).sendKeys("Gartenplatz,  3");
+        $(By.xpath("//input[@name='addressZipCode']")).sendKeys("13355");
+        $(By.xpath("//input[@name='addressCity']")).sendKeys("Berlin");
+
+        $(By.xpath("//option[contains(text(),'+34')]")).click();
+        $(By.xpath("//input[@name='mobilePhone']")).sendKeys("314151514151615");
+
+        $(By.xpath("//input[@name='promotionCode']")).sendKeys("1212231232323");
+
+        $$(By.xpath("//input[contains(@id, 'camelot-checkbox')]")).get(0);
+        $$(By.xpath("//input[contains(@id, 'camelot-checkbox')]")).get(1);
+        $$(By.xpath("//input[contains(@id, 'camelot-checkbox')]")).get(2);
+        $$(By.xpath("//input[contains(@id, 'camelot-checkbox')]")).get(3);
+
+
+//     (By.cssSelector("//button[@id='registration-save-button']")).click();
+
+
+
+
+
     }
 }
